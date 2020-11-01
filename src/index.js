e75549e63e24a503ced4ea4ae73fb2f513074485
Liu <q1697761819@qq.com>
2020-11-02 00:29:38 +0800

    A

--- src/index.js
+++ src/index.js
@@ -1,39 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './404.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
 
 
 function component() {
   const element = document.createElement('div');
-
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash，现在通过一个 script 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
-
-  console.log(toml.title);
-  console.log(toml.owner.name);
-
-  console.log(yaml.title);
-  console.log(yaml.owner.name);
-  console.log(json.title);
-  console.log(json.owner.name);
-
   return element;
 }
 
